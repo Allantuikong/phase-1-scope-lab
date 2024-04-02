3ee6789cd1eae05445eb47196e81d95cc7bf00c2
Allan Kipkorir <allantuix@gmail.com>
2024-04-02 13:26:22 +0300

    Completed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+};
+upperCaseCustomerName();
+
+// setBestCustomer()
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = "not bob";
+};
+setBestCustomer()
+
+// overwriteBestCustomer()
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+};
+overwriteBestCustomer();
+
+// changeLeastFavoriteCustomer()
+const leastFavoriteCustomer = "Angry customers";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = " Proud customers";
+};
+console.log(changeLeastFavoriteCustomer());
+
+
